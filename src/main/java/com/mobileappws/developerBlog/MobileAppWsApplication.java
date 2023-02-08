ea7080ec39a5f21dec671b00b8045bbf42f084ac
raihanuddin561 <raihanuddin561@gmail.com>
2023-02-08 21:54:57 +0600

    eureka client added

--- src/main/java/com/mobileappws/developerBlog/MobileAppWsApplication.java
+++ src/main/java/com/mobileappws/developerBlog/MobileAppWsApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableAsync
+@EnableEurekaClient
 public class MobileAppWsApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
